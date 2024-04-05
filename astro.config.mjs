80d3e1776f7e7c5c7fa64f00d4ecf87a6921eb23
Kevin Griffin <kevin@kevgriffin.com>
2024-04-05 09:05:49 -0400

    prospectus redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  redirects: {
+    "/prospectus": {
+      status: 302, // Use 301 for permanent redirects
+      destination:
+        "https://drive.google.com/file/d/1Pt1AwixVbP1C9HceFoaE28HB6f2MbH3P/view",
+    },
+  },
 });
